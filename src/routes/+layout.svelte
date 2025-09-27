550f825da8c68ed28642ce680337320a6fa2531a
harvanchik <jh6@harvhouse.com>
2025-09-27 14:02:39 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import 'virtual:pwa-assets/head';
+
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<main>
+		{@render children()}
+	</main>
+</div>
