f6288593a6f95cf672591ab8548644de0805ac78
harvanchik <66141166+harvanchik@users.noreply.github.com>
2026-02-05 11:55:54 -0600

    fix: vite websocket connection failed issue

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,3 +1,11 @@
+import { dev } from '$app/environment';
+
 if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('/service-worker.js');
+	if (dev) {
+		navigator.serviceWorker.getRegistrations().then((regs) => {
+			regs.forEach((reg) => reg.unregister());
+		});
+	} else {
+		navigator.serviceWorker.register('/service-worker.js', { type: 'module' });
+	}
 }
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,10 +1,11 @@
 import { dev } from '$app/environment';
+import type { Handle } from '@sveltejs/kit';
 
-export function handle({ event, resolve }) {
+export const handle: Handle = async ({ event, resolve }) => {
 	// Handle Chrome DevTools project settings request
 	if (dev && event.url.pathname === '/.well-known/appspecific/com.chrome.devtools.json') {
 		return new Response('Go away, Chrome DevTools!', { status: 404 });
 	}
 
 	return resolve(event);
-}
+};
